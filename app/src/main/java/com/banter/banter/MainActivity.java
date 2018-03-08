f6b25122388e2e90f3c965e352d7ac544a2ffba8
Evan Carlin <evan.carlin@slalom.com>
2018-03-08 15:36:31 -0800

    Adapter working

--- app/src/main/java/com/banter/banter/MainActivity.java
+++ app/src/main/java/com/banter/banter/MainActivity.java
@@ -1,185 +1,100 @@
 package com.banter.banter;
 
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.banter.banter.adapter.AccountAdapter;
 import com.banter.banter.model.Account;
-import com.banter.banter.viewModel.MainActivityViewModel;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-
-import java.util.Collections;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
     private static final String TAG = "MainActivity";
-    private static final int RC_SIGN_IN = 9001;
 
-    private FirebaseFirestore mFirestore;
-    private Query mQuery;
-
-    private AccountAdapter mAdapter;
-
-    private MainActivityViewModel mViewModel;
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
 
     @BindView(R.id.recycler_accounts)
-    RecyclerView mAccountsRecycler;
+    RecyclerView accountList;
 
-    @BindView(R.id.view_empty)
-    ViewGroup mEmptyView;
-
-    private FirestoreRecyclerAdapter adapter;
+    private FirebaseFirestore db;
+    private AccountAdapter accountAdapter;
+    LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        init();
+        getAccountList();
+    }
 
-        mViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+    private void init(){
+        linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        accountList.setLayoutManager(linearLayoutManager);
+        db = FirebaseFirestore.getInstance();
+    }
 
-        // Enable Firestore logging
-        FirebaseFirestore.setLoggingEnabled(true);
+    private void getAccountList(){
+        Query query = db.collection("Accounts");
 
-        // Firestore
-        mFirestore = FirebaseFirestore.getInstance();
+        FirestoreRecyclerOptions<Account> response = new FirestoreRecyclerOptions.Builder<Account>()
+                .setQuery(query, Account.class)
+                .build();
 
-        // Get users accounts
-        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
-        String uid = mUser.getUid();
-        DocumentReference docRef = mFirestore.collection("Accounts").document(uid);
-
-        mQuery = mFirestore.collection("Accounts");
-
-//        // RecyclerView
-        mAdapter = new AccountAdapter(mQuery) {
+        accountAdapter = new AccountAdapter(response) {
             @Override
-            protected void onDataChanged() {
-                // Show/hide content if the query returns empty.
-                if (getItemCount() == 0) {
-                    mAccountsRecycler.setVisibility(View.GONE);
-                    mEmptyView.setVisibility(View.VISIBLE);
-                } else {
-                    mAccountsRecycler.setVisibility(View.VISIBLE);
-                    mEmptyView.setVisibility(View.GONE);
+            public void onDataChanged() {
+                Log.d(TAG, "DataChangedCalled");
+                Log.d(TAG, "Count: "+getItemCount());
+                if(getItemCount() == 0) {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+                else {
+                    progressBar.setVisibility(View.GONE);
                 }
             }
-
-            @Override
-            protected void onError(FirebaseFirestoreException e) {
-                // Show a snackbar on errors
-                Snackbar.make(findViewById(android.R.id.content),
-                        "Error: check logs for info.", Snackbar.LENGTH_LONG).show();
-            }
         };
+        accountAdapter.notifyDataSetChanged();
+        accountList.setAdapter(accountAdapter);
+    }
 
-        mAccountsRecycler.setLayoutManager(new LinearLayoutManager(this));
-        mAccountsRecycler.setAdapter(mAdapter);
+    public class AccountsHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.account_item_name)
+        TextView textName;
 
+        public AccountsHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
-        // Start sign in if necessary
-        if (shouldStartSignIn()) {
-            startSignIn();
-            return;
-        }
+        accountAdapter.startListening();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-//        if (mAdapter != null) {
-//            mAdapter.stopListening();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-            mViewModel.setIsSigningIn(false);
-
-            if (resultCode != RESULT_OK) {
-                if (response == null) {
-                    // User pressed the back button.
-                    finish();
-                } else if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    showSignInErrorDialog(R.string.message_no_network);
-                } else {
-                    showSignInErrorDialog(R.string.message_unknown);
-                }
-            }
-        }
-    }
-
-    private boolean shouldStartSignIn() {
-        return (!mViewModel.getIsSigningIn() && FirebaseAuth.getInstance().getCurrentUser() == null);
-    }
-
-    private void startSignIn() {
-        // Sign in with FirebaseUI
-        Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
-                .setAvailableProviders(Collections.singletonList(
-                        new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build())) //TODO: Fix deprecation warnings
-                .setIsSmartLockEnabled(false)
-                .build();
-
-        startActivityForResult(intent, RC_SIGN_IN);
-        mViewModel.setIsSigningIn(true);
-    }
-
-    private void showSignInErrorDialog(@StringRes int message) {
-        AlertDialog dialog = new AlertDialog.Builder(this)
-                .setTitle(R.string.title_sign_in_error)
-                .setMessage(message)
-                .setCancelable(false)
-                .setPositiveButton(R.string.option_retry, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        startSignIn();
-                    }
-                })
-                .setNegativeButton(R.string.option_exit, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        finish();
-                    }
-                }).create();
-
-        dialog.show();
+        accountAdapter.stopListening();
     }
 
 }
--- app/src/main/java/com/banter/banter/adapter/AccountAdapter.java
+++ app/src/main/java/com/banter/banter/adapter/AccountAdapter.java
@@ -1,16 +1,23 @@
 package com.banter.banter.adapter;
 
 import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.banter.banter.MainActivity;
 import com.banter.banter.R;
 import com.banter.banter.model.Account;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.Query;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,41 +25,47 @@ import butterknife.ButterKnife;
 /**
  * Created by evan.carlin on 3/8/2018.
  */
-public class AccountAdapter extends FirestoreAdapter<AccountAdapter.ViewHolder>{
-    public AccountAdapter(Query query) {
-        super(query);
+
+public class AccountAdapter extends FirestoreRecyclerAdapter<Account, AccountAdapter.AccountsHolder>{
+    public static final String TAG = "AccountAdapter";
+
+    private FirestoreRecyclerOptions<Account> resposne;
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See
+     * {@link FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public AccountAdapter(FirestoreRecyclerOptions options) {
+        super(options);
+        resposne = options;
     }
+
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        return new ViewHolder(inflater.inflate(R.layout.item_account, parent, false));
+    protected void onBindViewHolder(AccountsHolder holder, int position, Account model) {
+        Log.e(TAG, "************** onBindViewHolderCalled");
+        holder.textName.setText(model.getName());
     }
 
+    @NonNull
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        throw new UnsupportedOperationException("The account adapter tried to call onBindViewholder!!!!!!!");
-//        holder.bind(getSnapshot(position), mListener);
+    public AccountsHolder onCreateViewHolder(@NonNull ViewGroup group, int viewType) {
+        View view = LayoutInflater.from(group.getContext())
+                .inflate(R.layout.item_account, group, false);
+
+        return new AccountsHolder(view);
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
 
+
+    public static class AccountsHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.account_item_name)
-        TextView nameView;
+        TextView textName;
 
-
-        public ViewHolder(View itemView) {
+        public AccountsHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
-
-        public void bind(final DocumentSnapshot snapshot) {
-
-            Account restaurant = snapshot.toObject(Account.class);
-            Resources resources = itemView.getResources();
-
-            nameView.setText(restaurant.getName());
-        }
-
     }
 }
